774c8d4f06635fa029b612a5e9b83c90c02b046a
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-09 00:22:17 +0430

    test for DarkRoastWithMochaAndWhipTests added

--- src/DarkRoastWithMochaAndWhipTests.java
+++ src/DarkRoastWithMochaAndWhipTests.java
@@ -2,4 +2,9 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DarkRoastWithMochaAndWhipTests {
+    @Test
+    void operationTest() {
+        Beverage beverage = new Whip(new Mocha(new DarkRoast()));
+        assertEquals("DarkRoast Add Mocha Add Whip", beverage.Operation());
+    }
 }
