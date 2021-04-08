f526d6c7f23247bf3cbf54117b8e39e8a3163af3
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-09 00:05:43 +0430

    operation added to whip

--- src/Whip.java
+++ src/Whip.java
@@ -2,4 +2,11 @@ public class Whip extends Decorator {
     public Whip(Beverage beverage) {
         super(beverage);
     }
+
+    @Override
+    public String Operation() {
+        return super.Operation() + " " + addBehavior();
+    }
+
+
 }
