d1afeccbed045a60537219afcd28bfba46a8f3f0
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-08 23:59:17 +0430

    operation overrided function added to Mocha

--- src/Mocha.java
+++ src/Mocha.java
@@ -2,4 +2,11 @@ public class Mocha extends Decorator {
     public Mocha(Beverage beverage) {
         super(beverage);
     }
+
+    @Override
+    public String Operation() {
+        return super.Operation() + " " + addBehavior();
+    }
+
+    
 }
