02995e4d0e7511e742939725f358a6826da37967
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-09 14:08:21 +0430

    getDescription added in Soy

--- src/Soy.java
+++ src/Soy.java
@@ -2,4 +2,18 @@ public class Soy extends Decorator {
     public Soy(Beverage beverage) {
         super(beverage);
     }
+
+    @Override
+    public String Operation() {
+        return super.Operation() + " " + addBehavior();
+    }
+
+    private String addBehavior() {
+        return "Add Soy";
+    }
+
+    public String getDescription() {
+        return super.getDescription() + " " + addBehavior();
+    }
+
 }
