87c2fba06880a3a9921bdae0a1e2c574adf9248e
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-09 14:00:53 +0430

    getDescription added in DarkRoast

--- src/DarkRoast.java
+++ src/DarkRoast.java
@@ -1,6 +1,11 @@
+DarkRoast.java:
+
 public class DarkRoast implements Beverage {
     @Override
     public String Operation() {
+        return "DarkRoast";
+    }
+    public String getDescription() {
         return "DarkRoast";
     }
 }
