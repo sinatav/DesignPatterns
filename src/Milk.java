a4398b906182bd18cada931ed1e49eccb5a23606
Sina <stavakkoli1999@gmail.com>
2021-04-09 12:54:23 +0430

    sidings added

---
+++ src/Milk.java
@@ -0,0 +1,5 @@
+public class Milk extends Decorator {
+    public Milk(Beverage beverage) {
+        super(beverage);
+    }
+}
---
+++ src/Soy.java
@@ -0,0 +1,5 @@
+public class Soy extends Decorator {
+    public Soy(Beverage beverage) {
+        super(beverage);
+    }
+}
