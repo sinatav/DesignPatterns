78d12a24ad7492aea75f41a9a808126c7b766b25
ellykhodaie <elahekhodaie@yahoo.com>
2021-04-09 14:07:33 +0430

    getDescription added in milk

--- src/Milk.java
+++ src/Milk.java
@@ -2,4 +2,17 @@ public class Milk extends Decorator {
     public Milk(Beverage beverage) {
         super(beverage);
     }
+
+    @Override
+    public String Operation() {
+        return super.Operation() + " " + addBehavior();
+    }
+
+    private String addBehavior() {
+        return "Add Milk";
+    }
+
+    public String getDescription() {
+        return super.getDescription() + " " + addBehavior();
+    }
 }
